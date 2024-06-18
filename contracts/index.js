0d68ae06c3a289cc21b2f12893fd422a46e22f56
Nicolas Villanueva <nicolasvillanueva@msn.com>
2024-06-18 19:38:29 +0100

    debug

--- contracts/index.js
+++ contracts/index.js
@@ -1,10 +1,15 @@
 const ganache = require("ganache-cli");
 
-const options = { wallet: { accounts: "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110,800000000000"}};
+const options = {
+  wallet: {
+    accounts:
+      "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110,50000000000000000000",
+  },
+};
 const server = ganache.server(options);
-const PORT = 8051; // 0 means any available port
+const PORT = 8050; // 0 means any available port
 console.log(`Starting server with port ${PORT}`);
-server.listen(PORT, async err => {
+server.listen(PORT, async (err) => {
   if (err) throw err;
 
   console.log(`ganache listening on port ${server.address().port}...`);
