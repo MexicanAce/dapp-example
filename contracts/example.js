18379053b3a958ea9a4cbbdbe603ea61c04b4d46
Nicolas Villanueva <nicolasvillanueva@msn.com>
2024-06-20 15:52:42 +0100

    start frontend on startup

--- contracts/example.js
+++ contracts/example.js
@@ -1,4 +1,16 @@
 const { Web3 } = require('web3');
-var client = new Web3("https://zksync-devnet.nethermind.io")
-client.eth.getAccounts().then(console.log);
-client.eth.getBalance("0x36615Cf349d7F6344891B1e7CA7C72883F5dc049").then(console.log);
+
+const url = "http://localhost:8050";
+const richWalletAddress = "0x36615Cf349d7F6344891B1e7CA7C72883F5dc049";
+var client = new Web3(url);
+
+console.log(`Local node is confirmed running at ${url}`);
+client.eth.getChainId().then(x => {
+    console.log(`Chain ID: ${x}`);
+});
+client.eth.getAccounts().then(x => {
+    console.log(`Accounts available: ${x}`);
+});
+client.eth.getBalance(richWalletAddress).then(x => {
+    console.log(`Balance of ${richWalletAddress}: ${x}`);
+});
--- contracts/index.js
+++ contracts/index.js
@@ -3,13 +3,13 @@ const ganache = require("ganache-cli");
 const options = {
   accounts: [
     {
-      "balance": 0x50000000000000000000,
+      "balance": "0x56BC75E2D63100000",
       "secretKey": "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110",
     },
   ],
 };
 const server = ganache.server(options);
-const PORT = 8050; // 0 means any available port
+const PORT = 8051; // 0 means any available port
 console.log(`Starting server with port ${PORT}`);
 server.listen(PORT, async (err) => {
   if (err) throw err;
